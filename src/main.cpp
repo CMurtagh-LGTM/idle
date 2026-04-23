5658d7a92cb2de771362748a9a03b3f87109fab0
cameron <cameron.murtagh00@gmail.com>
2026-04-23 14:47:24 +1000

    first window

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include "engine/context.hpp"
+
+int main() {
+
+    engine::Context context;
+
+    context.start();
+
+}
---
+++ subprojects/engine/include/engine/context.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace engine {
+
+class Context {
+public:
+  Context();
+  void start();
+};
+
+} // namespace engine
